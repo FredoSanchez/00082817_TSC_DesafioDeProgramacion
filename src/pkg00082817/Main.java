c4393eb50feb4aca59ff4eb32f0b0544f6d22151
Fredy Sánchez <00082817@uca.edu.sv>
2020-07-03 15:57:03 -0600

    Desafio terminado

---
+++ src/pkg00082817/Main.java
@@ -0,0 +1,23 @@
+package pkg00082817;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Fredy Sanchez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home home = new Home();
+        home.setVisible(true);
+    }
+    
+}
